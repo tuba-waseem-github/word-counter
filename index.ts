5da0d779a31521de55e522ebb7779a213a74960d
tuba waseem <tubaw891@gmail.com>
2024-05-18 13:42:34 -0700

    this is a word counter

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+//This line is called a shabang, it tells the os to run it with node.js
+
+// import the "inquirer" module, which is command line interface for Node.js
+import inquirer from "inquirer";
+// Declare a constant `answere` and asign it to the result of the `inquirer.prompt` function
+const answere:{
+    sentence:string
+} = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the words: ",
+    }
+])
+
+const words = answere.sentence.trim().split(" ")
+
+//print the aaray of words to the console
+console.log(words);
+
+//print the word count of the sentence to the console
+console.log(`your sentence is word count is ${words.length}`);
